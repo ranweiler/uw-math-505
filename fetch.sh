5fe6044cc43fbc2ddd3cd49d2cc7fe1f65c1b67b
Joe Ranweiler <josephkennethranweiler@gmail.com>
2016-12-27 05:34:21 -0800

    Add fetch script

---
+++ fetch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+url='http://www.math.washington.edu/~mcgovern/505wi17'
+files=$(curl "${url}/" 2>/dev/null | pcregrep -o1 '<a.*>(.*tex)</a>')
+
+mkdir -p raw
+pushd raw >/dev/null
+for f in $files
+do
+  curl -O "${url}/${f}"
+done
+popd >/dev/null
